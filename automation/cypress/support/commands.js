a2d22cfb5f2b5bf2e987138b1602d3a5600510de
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-11-17 11:21:04 +0530

    custom commands updated

--- automation/cypress/support/commands.js
+++ automation/cypress/support/commands.js
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// custom commands
+Cypress.Commands.add("login", (username = "Admin", password = "admin123") => {
+  cy.visit("/");
+  cy.get("input[name='username']").clear().type(username);
+  cy.get("input[name='password']").clear().type(password);
+
+  cy.get(".orangehrm-login-button").click();
+  cy.get(".oxd-topbar-header-breadcrumb-module").should("contain", "Dashboard");
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.get(".oxd-userdropdown").click();
+  cy.get(".oxd-dropdown-menu li:nth-child(4) > a").contains("Logout").click();
+});
